dc85cf4b51c33ca094dd452bd12e81df86f34ff9
theo <tkleinen@gmail.com>
2017-09-21 15:19:43 +0200

    Creates ManualSeries objects

--- delft/management/commands/hand.py
+++ delft/management/commands/hand.py
@@ -6,12 +6,11 @@ Created on Dec 6, 2014
 import csv, datetime
 from optparse import make_option
 from django.core.management.base import BaseCommand
-from acacia.data.models import ProjectLocatie, MeetLocatie, Series, ManualSeries
+from acacia.data.models import ProjectLocatie, MeetLocatie, ManualSeries
 from acacia.meetnet.models import Well,Screen
 from django.db.models import Q
 from django.contrib.auth.models import User
 import pytz
-from tz import NL as WinterTijd
 
 class Command(BaseCommand):
     args = ''
@@ -26,7 +25,7 @@ class Command(BaseCommand):
         
     def handle(self, *args, **options):
         fname = options.get('fname')
-        CET=pytz.timezone('Europe/Amsterdam')
+        CET=pytz.timezone('CET')
         user=User.objects.get(username='theo')
         if fname:
             with open(fname,'r') as f:
@@ -51,18 +50,12 @@ class Command(BaseCommand):
                             print 'Reference point for screen %s not available' % screen
                             continue
                         nap = screen.refpnt - depth
+                        
                         date = datetime.datetime.strptime(datumtijd,'%d/%m/%Y %H:%M')
                         date = CET.localize(date)
-                        date = date.astimezone(WinterTijd)
                         series_name = '%s HAND' % mloc.name
-                        try:
-                            series = mloc.series_set.get(name=series_name)
-                        except:
-                            series = ManualSeries.create(name=series_name, mlocatie=mloc, description='Handpeiling', unit='m NAP', type='scatter', user=user)
-                        pt, created = series.datapoints.get_or_create(date=date,defaults={'value': nap})
-                        if not created:
-                            pt.value = nap
-                            pt.save()
+                        series,created = ManualSeries.objects.get_or_create(name=series_name,mlocatie=mloc,defaults={'description':'Handpeiling', 'timezone':'CET', 'unit':'m NAP', 'type':'scatter', 'user':user})
+                        pt, created = series.datapoints.update_or_create(date=date,defaults={'value': depth})
                         print screen, pt.date, pt.value
                     except Well.DoesNotExist:
                         print 'Well %s not found' % NITG
