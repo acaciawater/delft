6689ef31adaac21b04d868e8150c6c0903342c91
theo <tkleinen@gmail.com>
2017-09-21 15:20:18 +0200

    exports 5 wells Delft-Noord only

--- delft/management/commands/exporteer.py
+++ delft/management/commands/exporteer.py
@@ -9,11 +9,16 @@ class Command(BaseCommand):
     help = 'Export all series'
         
     fldr = './export'
+    
 
     def handle(self, *args, **options):
+        screens = ['B37E0275/001','B37E0312/004','B37E3473/004','B37E3502/001','B37E3507/001']
         if not os.path.exists(self.fldr):
             os.makedirs(self.fldr)
         for s in Series.objects.all():
+            loc = s.meetlocatie()
+            if not loc or not loc.name in screens:
+                continue
             if s.parameter:
                 name = unicode(s.parameter)
             else: 
