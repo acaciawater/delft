bbcd9167aa30157a75ca0162152b305c16734309
theo <tkleinen@gmail.com>
2017-12-18 22:05:20 +0100

    comment out screen selection for DSM only

--- delft/management/commands/exporteer.py
+++ delft/management/commands/exporteer.py
@@ -12,13 +12,13 @@ class Command(BaseCommand):
     
 
     def handle(self, *args, **options):
-        screens = ['B37E0275/001','B37E0312/004','B37E3473/004','B37E3502/001','B37E3507/001']
+#        screens = ['B37E0275/001','B37E0312/004','B37E3473/004','B37E3502/001','B37E3507/001']
         if not os.path.exists(self.fldr):
             os.makedirs(self.fldr)
         for s in Series.objects.all():
-            loc = s.meetlocatie()
-            if not loc or not loc.name in screens:
-                continue
+#            loc = s.meetlocatie()
+#            if not loc or not loc.name in screens:
+#                continue
             if s.parameter:
                 name = unicode(s.parameter)
             else: 
