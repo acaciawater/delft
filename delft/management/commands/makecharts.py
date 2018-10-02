eba6a4e01b6c259950782f40f328b31173f0abd5
theo <tkleinen@gmail.com>
2018-10-02 12:31:38 +0200

    makecharts management command allows for filtering on owner and year

--- delft/management/commands/makecharts.py
+++ delft/management/commands/makecharts.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import os
 import pytz
 from acacia.data.util import slugify
+from django.shortcuts import get_object_or_404
 
 class Command(BaseCommand):
     help = 'maak grafiekjes voor 2017'
@@ -19,18 +20,47 @@ class Command(BaseCommand):
                 dest = 'dest',
                 default = '.',
                 help = 'destination folder')
+
         parser.add_argument('-s', '--skip',
                 action='store_true',
                 dest = 'skip',
                 default = False,
-                help = 'skip well charts')
+                help = 'skip screen charts')
+
+        parser.add_argument('-o', '--owner',
+                action='store',
+                dest = 'owner',
+                default = 'PZH',
+                help = 'filter wells by owner')
+        
+        parser.add_argument('-w', '--well',
+                action='store',
+                dest = 'well',
+                default = False,
+                help = 'id of well to make charts for')
+
+        parser.add_argument('-b', '--begin',
+                action='store',
+                dest = 'begin',
+                default = None,
+                help = 'first year')
+
+        parser.add_argument('-e', '--end',
+                action='store',
+                dest = 'end',
+                default = None,
+                help = 'last year')
                         
     def handle(self, *args, **options):
         folder = options.get('dest')
-        skip = options.get('skip')
-        tz = pytz.timezone('Etc/GMT-1')
-        start=datetime(2017,1,1,tzinfo=tz)
-        stop=datetime(2017,12,31,tzinfo=tz)
+        noscreen = options.get('noscreen')
+        begin = options.get('begin')
+        end = options.get('end')
+        tz = pytz.timezone('CET')
+        pk = options.get('well')
+        owner=options.get('owner')
+        start=datetime.datetime(int(begin),1,1,tzinfo=tz) if begin else None
+        stop=datetime.datetime(int(end),12,31,tzinfo=tz) if end else None
         if not os.path.exists(folder):
             os.makedirs(folder)
         
@@ -38,20 +68,19 @@ class Command(BaseCommand):
         #screens = ['B37E0275/001','B37E0312/004','B37E3473/004','B37E3502/001','B37E3507/001']
 
         #for w in Well.objects.filter(name__in=wells):
-        for w in Well.objects.all():
-            if not skip:
-                data = chart_for_well(w)
-                filename = os.path.join(folder,w.nitg + '.png')
-                print filename
-                with open(filename,'wb') as png:
-                    png.write(data)
+        queryset = [get_object_or_404(Well,pk=pk)] if pk else Well.objects.filter(owner=owner)
+        for w in queryset:
+            data = chart_for_well(w,start=start,stop=stop)
+            filename = os.path.join(folder,slugify(unicode(w)) + '.png')
+            print filename
+            with open(filename,'wb') as png:
+                png.write(data)
+            if not noscreen:
+                for s in w.screen_set.all():
+                    data = chart_for_screen(s,start=start,stop=stop)
+                    filename = os.path.join(folder,slugify(unicode(s)) + '.png')
+                    print filename
+                    with open(filename,'wb') as png:
+                        png.write(data)
 
-            for s in w.screen_set.all():
-                name = unicode(s)
-                #if name in screens:
-                data = chart_for_screen(s,start,stop,loggerpos=False)
-                filename = os.path.join(folder,slugify(name) + '.png')
-                print filename
-                with open(filename,'wb') as png:
-                    png.write(data)
             
