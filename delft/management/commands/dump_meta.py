b8cf7255e0d2d2b29d28b0803e52604c2a5163d8
theo <tkleinen@gmail.com>
2019-07-23 14:19:24 +0200

    adds start/stop/count

--- delft/management/commands/dump_meta.py
+++ delft/management/commands/dump_meta.py
@@ -11,8 +11,15 @@ class Command(BaseCommand):
     help = 'maakt metadata dump'
                         
     def handle(self, *args, **options):
-        print("put,nitg,maaiveld,lengtegraad,breedtegraad,filternummer,bovenkantbuis,bovenkantfilter,onderkantfilter")            
+        print("put,nitg,maaiveld,lengtegraad,breedtegraad,filternummer,bovenkantbuis,bovenkantfilter,onderkantfilter,start,stop,aantal")            
         for s in Screen.objects.order_by('well','nr'):
             w=s.well
             coords = w.latlon()
-            print(','.join(map(str,[w.name,w.nitg,w.maaiveld,coords.x,coords.y,s.nr,s.refpnt,s.top,s.bottom])))
+            points = s.find_series().datapoints
+            if points.exists():
+                start = points.earliest('date').date.date()
+                stop = points.latest('date').date.date()
+                count = points.count()
+            else:
+                start = stop = count = None
+            print(','.join(map(str,[w.name,w.nitg,w.maaiveld,coords.x,coords.y,s.nr,s.refpnt,s.top,s.bottom,start,stop,count or 0])))
