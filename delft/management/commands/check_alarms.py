59ef0719b722d74cfdc4eca92d194c6d0f199600
theo <tkleinen@gmail.com>
2021-01-24 17:29:05 +0100

    download data and update charts

--- delft/management/commands/check_alarms.py
+++ delft/management/commands/check_alarms.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from django.db.models.aggregates import Count
 import logging
 from datetime import timedelta
+from acacia.meetnet.actions import make_wellcharts
+from acacia.meetnet.models import Well
 logger = logging.getLogger(__name__)
             
 class Command(BaseCommand):
@@ -15,12 +17,29 @@ class Command(BaseCommand):
                 dest = 'notify',
                 help = 'Notify receivers by email'
         )
+        parser.add_argument('-d','--download',
+                action = 'store_true',
+                dest = 'download',
+                help = 'Download new data before checks'
+        )
         parser.add_argument('-s','--start',
                 dest = 'start',
                 help = 'starting time for checking alarm conditions (default=24 hours ago)'
         )
-    
+
+    def download_and_update(self, series):
+        try:
+            ds = series.datasource()
+            if ds.download():
+                data = ds.get_data(start=series.tot())
+                if series.update(data=data):
+                    well = Well.objects.get(name=series.projectlocatie().name)
+                    make_wellcharts(None,None,[well])
+        except Exception as e:
+            logger.error(e)
+                
     def handle(self, *args, **options):
+        download = options.get('download')
         notify = options.get('notify')
         now = timezone.now().replace(minute=0,second=0,microsecond=0)
         start = options.get('start')
@@ -31,6 +50,8 @@ class Command(BaseCommand):
         logger.debug('checking {} time series for alarms'.format(queryset.count()))
         count = 0
         for series in queryset:
+            if download:
+                self.download_and_update(series)
             for alarm in series.alarm_set.filter(active=True):
                 events = alarm.inspect(notify=notify,start=start,stop=now)
                 if events:
