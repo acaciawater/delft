1f6e1a4dd53489152d1d535ab97b29ccd90532ed
theo <tkleinen@gmail.com>
2021-01-24 17:40:03 +0100

    update script modified

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 source ../env/bin/activate
-./manage.py update >> logs/update.log
-./manage.py check_alarms --notify >> logs/update.log
+./manage.py check_alarms --download --notify >> logs/update.log
